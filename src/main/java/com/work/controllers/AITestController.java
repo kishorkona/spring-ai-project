b9763cba8b639fe2087115d605c91b6a4fa8e4d5
kishorkona <kishorkona@gmail.com>
2026-01-15 23:36:00 -0500

    modify

--- src/main/java/com/work/controllers/AITestController.java
+++ src/main/java/com/work/controllers/AITestController.java
@@ -1,9 +1,11 @@
 package com.work.controllers;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,12 +27,30 @@ public class AITestController {
     @GetMapping("/ai/chat")
     public String generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
         System.out.println("Kishor::: message: " + message);
-
         return chatClient.prompt()
                 .user(message)
                 .call()
                 .content();
+    }
 
-        //return "kishor";
+    @GetMapping("/getPopulation/{countryName}")
+    public String getPopulation(@PathVariable("countryName") String countryName) {
+        if(!countryName.isBlank() &&  !countryName.isEmpty()) {
+            String promptText = "What is the current population of "+countryName+"? Respond with only the number, with no additional text, words, or formatting.";
+
+            OllamaOptions options = OllamaOptions.builder()
+                    .temperature(0.0d)
+                    .topP(1.0d)
+                    .seed(42)
+                    .build();
+
+            String population = chatClient.prompt()
+                    .user(promptText)
+                    .options(options)
+                    .call()
+                    .content();
+            return population;
+        }
+        return "Country Name is blank";
     }
 }
